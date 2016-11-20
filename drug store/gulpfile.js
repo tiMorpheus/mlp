b9ceb4e1ea87302e3082aa4bd89031c427898508
tiMorpheus <timur10645@icloud.com>
2016-11-20 17:59:05 +0200

    верх меню

--- drug store/gulpfile.js
+++ drug store/gulpfile.js
@@ -5,7 +5,7 @@ var browserSync = require('browser-sync').create();
 
 
 gulp.task('sass', function(){
-    return gulp.src('app/sass/**/*.scss')
+    return gulp.src('app/scss/**/*.scss')
         .pipe(sass())   // Converts Sass to CSS with gulp-sass
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({
