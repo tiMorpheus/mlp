db132f79261674bd3adca5f8d1a1896c3fb89a7b
tiMorpheus <timur10645@icloud.com>
2016-11-24 15:00:11 +0200

    init

--- drug store/gulpfile.js
+++ drug store/gulpfile.js
@@ -1,21 +1,12 @@
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+
 var browserSync = require('browser-sync').create();
 
 
-gulp.task('sass', function(){
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(sass())   // Converts Sass to CSS with gulp-sass
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
 
-
-gulp.task('watch',['browserSync', 'sass'] ,function(){
-    gulp.watch('app/scss/**/*.scss', ['sass']);
+gulp.task('watch',function(){
+    gulp.watch('app/css/**/*.css', browserSync.reload);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 });
